52ad11826c237a960cbed5c6bb78fc33dc80db07
EVBLOOD <saad.akllam@gmail.com>
2022-11-08 22:35:07 +0100

    class parser + constructors are ready --- parsing start

---
+++ parser.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sakllam <sakllam@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 22:07:55 by sakllam           #+#    #+#             */
+/*   Updated: 2022/11/08 22:34:01 by sakllam          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <string>
+#include <fstream>
+
+class parser
+{
+    private:
+        std::ifstream config;
+        
+    public:
+        parser(const std::string &filename);
+        ~parser();
+};
+
+parser::parser(const std::string &filename)
+{
+    config.open(filename); // open file
+    if (config.is_open() == false ) // protection mode: file opened?
+    { // no ? okey
+        std::cout << "Some respect please ! \n";
+        exit(1);
+        // else throw an error;
+    }
+    if (config.peek() == EOF) // protection mode: nothing in the file
+    {// EOF > okey
+        std::cout << "Empty ---> \n";
+        exit (1);
+        // else throw an error;
+    }
+}
+
+parser::~parser()
+{
+    config.close();
+}
