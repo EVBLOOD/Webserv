a502ab6ddd7ed79d9e1a21415413366acd3815e6
Saad Akllam <sakllam@e2r6p15.1337.ma>
2022-11-09 17:55:03 +0100

    tokenizer done

--- parser.hpp
+++ parser.hpp
@@ -6,15 +6,19 @@
 /*   By: sakllam <sakllam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 22:07:55 by sakllam           #+#    #+#             */
-/*   Updated: 2022/11/09 16:20:22 by sakllam          ###   ########.fr       */
+/*   Updated: 2022/11/09 17:54:01 by sakllam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <cstdio>
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <list>
+#include "tokengen.hpp"
+#include <cctype>
 class parser
 {
     private:
@@ -22,7 +26,7 @@ class parser
         
     public:
         parser(const std::string &filename);
-        // tokens  &generate();
+        std::list<tokengen> generate(std::ifstream config);
         ~parser();
 };
 
@@ -41,7 +45,63 @@ parser::parser(const std::string &filename)
         exit (1);
         // else throw an error;
     }
-    // generate tokens! 
+}
+
+std::list<tokengen> parser::generate(std::ifstream config)
+{
+    std::list<tokengen> lexer;
+    std::string line;
+    int i;
+    int start;
+    while (getline(config, line))
+    {
+        i = 0;
+        while (isspace(line[i]))
+          i++;
+        while (line[i])
+        {
+            if (isspace(line[i]))
+            {
+                lexer.push_back(tokengen(WHITESPACE));
+                while (isspace(line[i]))
+                    i++;
+                continue;
+            }
+            if (line[i] == '"')
+            {
+                start = i;
+                while (line[i] && line[i] != '"')
+                    i++;
+                lexer.push_back(tokengen(QUOTES, line.substr(start, i)));
+            }
+            else if (line[i] == '\'')
+            {
+                start = i;
+                while (line[i] && line[i] != '\'')
+                    i++;
+                lexer.push_back(tokengen(QUOTES, line.substr(start, i)));
+            }
+            else if (line[i] == '{')
+                lexer.push_back(tokengen(OPENCURL));
+            else if (line[i] == '}')
+                lexer.push_back(tokengen(CLOSECURL));
+            else if (line[i] == ';')
+                lexer.push_back(tokengen(SEMICOLONS));
+            else if (line[i] == ':')
+                lexer.push_back(tokengen(COLON));
+            else
+            {
+                start = i;
+                while (line[i] && line[i] != '{' && line[i] != '}'\
+                    && line[i] != ';' && line[i] != ':' && line[i] != '\'' && line[i] != '"'  && !isspace(line[i]))
+                    i++;
+                lexer.push_back(tokengen(WORD, line.substr(start, i)));
+            }
+            if (line[i])
+                i++;
+        }
+    }
+    return lexer;
 }
 
 parser::~parser()
