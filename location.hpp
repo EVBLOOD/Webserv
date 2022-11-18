8df36e90bd64f2b04ce9aeca9b15f85d5661b345
Saad Akllam <sakllam@e2r5p11.1337.ma>
2022-11-18 18:01:39 +0100

    some refactoring needed

--- location.hpp
+++ location.hpp
@@ -6,7 +6,7 @@
 /*   By: sakllam <sakllam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 22:58:39 by sakllam           #+#    #+#             */
-/*   Updated: 2022/11/15 18:44:50 by sakllam          ###   ########.fr       */
+/*   Updated: 2022/11/18 12:59:32 by sakllam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,27 +54,6 @@ class location
         template<int>
             void set(std::list<tokengen>::iterator &big, std::list<tokengen>::iterator &end); // turn this to refrence
         void execute(int i, std::list<tokengen>::iterator &big, std::list<tokengen>::iterator &end);
-        // template<>
-        // void set<setallow_methods>(std::list<tokengen>::iterator big, std::list<tokengen>::iterator end);
-        // template<>
-        //     void set<setfastcgi_pass>(std::list<tokengen>::iterator big, std::list<tokengen>::iterator end);
-        // template<>
-        //     void set<setindex>(std::list<tokengen>::iterator big, std::list<tokengen>::iterator end);
-        // template<>
-        //     void set<setreturn>(std::list<tokengen>::iterator big, std::list<tokengen>::iterator end);
-        // template<>
-        //     void set<setautoindex>(std::list<tokengen>::iterator big, std::list<tokengen>::iterator end);
-        // template<>
-        //     void set<setupload_enable>(std::list<tokengen>::iterator big, std::list<tokengen>::iterator end);
-        // template<>
-        //     void set<setupload_store>(std::list<tokengen>::iterator big, std::list<tokengen>::iterator end);
-        // void setallow_methods(std::string x);
-        // void setfastcgi_pass(std::string x);
-        // void setindex(std::string x);
-        // void setreturn(std::string x);
-        // void setautoindex(std::string x);
-        // void setupload_enable(std::string x);
-        // void setupload_store(std::string x);
 };
 
 void CURLWAIT(std::list<tokengen>::iterator &x, std::list<tokengen>::iterator &end, bool loc = false);
