aa490b1472506d411d4c8b70b3f2cfa71594ddc9
Saad Akllam <sakllam@e2r5p13.1337.ma>
2022-11-28 18:57:13 +0100

    multi sockets detection is done, work of is adding new when added is left

--- main.cpp
+++ main.cpp
@@ -51,9 +51,13 @@ This helps in manipulating options for the socket referred by the file descripto
  binds the socket to the address and port number specified in addr(custom data structure)
 */
 // #include "socket.hpp"
+#include <cstdlib>
+#include <malloc/_malloc.h>
 #include <netinet/in.h>
+#include <ostream>
 #include <string>
 #include <sys/_types/_size_t.h>
+#include <sys/_types/_socklen_t.h>
 #include <sys/poll.h>
 #include <sys/socket.h>
 #include <sys/syslimits.h>
@@ -65,94 +69,237 @@ This helps in manipulating options for the socket referred by the file descripto
 #include <iostream>
 #include <unistd.h>
 #include <fcntl.h>
+#include <vector>
+#include <cstring>
 typedef struct addrinfo infos;
 typedef struct sockaddr_storage storage;
-typedef struct pollfd spool;
+#include <sys/event.h>
+#include <sys/time.h>
+
+int create_socket_and_listen(std::string host, std::string port, int number)
+{
+    infos *addr;
+    infos hints;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_protocol = IPPROTO_TCP; // I think I can remove this one!
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    if (getaddrinfo(host.c_str(), port.c_str(), &hints, &addr) == -1)
+    {
+        std::cerr << "get info is joking!\n";
+        exit (1);
+    }
+    int s = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+    int enable = 1;
+    setsockopt(s, SOL_SOCKET, SO_REUSEADDR,  &enable, sizeof(enable));
+    if (bind(s, addr->ai_addr, addr->ai_addrlen) == -1)
+    {
+        std::cerr << "bind is joking!\n";
+        exit (1);
+    }
+    if (listen(s, number) == -1)
+    {
+        std::cerr << "listen is joking!\n";
+        exit (1);
+    }
+    freeaddrinfo(addr);
+    return s;
+}
+
+int init_kqueue(int number, int *data)
+{
+    struct kevent evSet;
+    int kq;
+    kq = kqueue();
+    if (kq == -1)
+    {
+        std::cerr << "kqueue is joking!\n";
+        exit (1);
+    }
+    for (int i = 0; i < number; i++)
+    {
+        EV_SET(&evSet, data[i], EVFILT_READ, EV_ADD, 0, 0, NULL);
+        if (kevent(kq, &evSet, 1, NULL, 0, NULL) == -1)
+        {
+            std::cerr << "kevent is joking!\n";
+            exit (1);
+        }
+    }
+    return (kq); // this may be an error!
+}
+ 
+ // at the last moment : I found this and it does everything => https://nima101.github.io/io_multiplexing
+void kJob(int kdata, int *data)
+{
+    struct kevent change, event;
+    struct kevent evSet;
+    while (true)
+    {
+        // EV_SET(evSet, sckfd, EVFILT_READ, EV_DELETE, 0, 0, 0); when needs to remove
+        int fd = kevent(kdata, &change, 1, &event, 1, NULL);
+        if (fd == -1)
+        {
+            std::cerr << " kevent before accept is joking!\n";
+            exit (1);
+        }
+        int rwfd = accept(event.ident, NULL, NULL);
+        if (rwfd == -1)
+        {
+            std::cerr << "accept is joking!\n";
+            exit (1);
+        }
+        std::cout << "Read?\n";
+        // write(rwfd, "200 1.1 OK\r\nLocation: / \r\nContent-Type text/html\r\n\r\n<h1>hello, world</h1>\r\n",strlen("200 1.1 OK\r\nLocation: / \r\nContent-Type text/html\r\n\r\n<h1>hello, world</h1>\r\n"));
+        // close(rwfd);
+        // }
+        // // exit (0);
+        // EV_SET(&evSet, rwfd, EVFILT_READ, EV_ADD, 0, 0, NULL);
+        // if (kevent(kdata, &evSet, 1, NULL, 0, NULL) == -1)
+        // {
+        //     std::cerr << "kevent is joking!\n";
+        //     exit (1);
+        // }
+        // char buffer[1025];
+        // // std::string request_text = std::string();
+        // int rec;
+        // // while (1)
+        // // {
+        // rec = recv(rwfd, &buffer, 1024, 0);
+        /*
+                   write("200 1.1 OK\r\nLocation: /\r\nContent-Type text/html\r\n<h1>hello, world</1>");
+        */
+        // send(int, const void *, size_t, int)
+        // if (rec == -1)
+        // {
+        //     std::cerr << "recv is joking!\n";
+        //     exit (1);
+        // }
+        // buffer[rec] = '\0';
+        // std::cout << buffer << "\n";
+    }
+}
 
 int main()
 {
-    infos info;
-    infos *ret[10];
-    spool data[10];
-    info.ai_protocol = IPPROTO_TCP;
-    info.ai_flags = AI_PASSIVE;
-    info.ai_family = AF_INET;
-    info.ai_socktype = SOCK_STREAM;
-    std::string y[10] = {"8081", "1037", "1125", "1319", "1331"};
-    for (int i = 0; i < 5; i++)
-    {
-        if (getaddrinfo("localhost", y[i].c_str(), &info, &(ret[i])) == -1)
-        {
-            std::cerr << "get info is joking!\n";
-            exit (1);
-        }
-        if ((data[i].fd = socket(ret[i]->ai_family, ret[i]->ai_socktype, ret[i]->ai_protocol)) == -1)
-        {
-            std::cerr << "socket is joking!\n";
-            exit (1);
-        }
-        // fcntl(data[i].fd, F_SETFL, O_NONBLOCK);
-        data[i].events = POLLIN | POLLOUT;
-        if (bind(data[i].fd, ret[i]->ai_addr, ret[i]->ai_addrlen) == -1)
-        {
-            std::cerr << "bind is joking!\n";
-            exit (1);
-        }
-        if (listen(data[i].fd, 5) == -1)
-        {
-            std::cerr << "listen is joking!\n";
-            exit (1);
-        }
-    }
-    int pollre;
-    int i;
-    storage x;
-    int rwfd;
-    socklen_t len = sizeof(storage);
-    int clnt = 5;
-    int crl = 0;
-    while (1)
-    {
-        std::cout << crl << '\n';
-        crl++;
-        pollre = poll(data, clnt, -1);
-        if (pollre == -1)
-        {
-            std::cerr << "pool is joking!\n";
-            exit (1);
-        }
-        else
-        {
-            for (i = 0; i < 5 ; i++){
-                if (data[i].revents == POLLIN || data[i].revents == POLLOUT)
-                {
-                    break;
-                }
-            }
-            if (i > 5)
-            {
-                std::cerr << "pool return is joking!\n";
-                exit (1);
-            }
-            rwfd = accept(data[i].fd, NULL, NULL);
-            if (rwfd == -1)
-            {
-                std::cerr << "accept is joking!\n";
-                exit (1);
-            }
-            char buffer[1000];
-            int rec = recv(rwfd, buffer, 1000, 0);
-            if (rec == -1)
-            {
-                std::cerr << "recv is joking!\n";
-                exit (1);
-            }
-            buffer[rec] = '\0';
-            std::cout << "\n------------------------------------------------------------------------------------" << std::endl;
-            std::cout << buffer;
-            std::cout << "\n------------------------------------------------------------------------------------" << std::endl;
-            close(rwfd);
-        }
+    int number = 5;
+    int data[number];
+    std::string y[5] = {"8081", "1037", "1125", "1319", "1331"}; // Number to change
 
-    }
+    for (int i = 0; i < number; i++)
+        data[i] = create_socket_and_listen("localhost", y[i], number);
+    int kdata;
+    kdata = init_kqueue(number, data);
+    kJob(kdata, data);
+    // ---------------------------------------------------------------------------------- TODO: <kqueue() code/> Done [ ] ---------------------------------------------------
+   
 }
+    // int pollre;
+    // int i;
+    // ---------------------------------------------------------------------------------- TODO: <Poll() code/> Done [√] ---------------------------------------------------
+    // storage x;
+    // int rwfd;
+    // socklen_t len = sizeof(storage);
+    // int clnt = 5;
+    // int crl = 0;
+    // while (1)
+    // {
+    //     std::cout << crl << '\n';
+    //     crl++;
+    //     pollre = poll(data, clnt, -1);
+    //     // clnt++;
+    //     if (pollre == -1)
+    //     {
+    //         std::cerr << "pool is joking!\n";
+    //         exit (1);
+    //     }
+    //     else
+    //     {
+    //         for (i = 0; i < 5 ; i++){
+    //             if (data[i].revents == POLLIN || data[i].revents == POLLOUT)
+    //             {
+    //                 break;
+    //             }
+    //         }
+    //         if (i > 5)
+    //         {
+    //             std::cerr << "pool return is joking!\n";
+    //             exit (1);
+    //         }
+    //         rwfd = accept(data[i].fd, NULL, NULL);
+    //         if (rwfd == -1)
+    //         {
+    //             std::cerr << "accept is joking!\n";
+    //             exit (1);
+    //         }
+    //         char buffer[1025];
+    //         // std::string request_text = std::string();
+    //         int rec;
+    //         // while (1)
+    //         // {
+    //         rec = recv(rwfd, &buffer, 1024, 0);
+    //         if (rec == -1)
+    //         {
+    //             std::cerr << "recv is joking!\n";
+    //             exit (1);
+    //         }
+    //         buffer[rec] = '\0';
+    //         std::cout << "\n------------------------------------------------------------------------------------" << std::endl;
+    //         std::cout << buffer;
+    //         std::cout << "\n------------------------------------------------------------------------------------" << std::endl;
+    //         std::vector<std::string> requestlines;
+    //         std::string s = buffer;
+
+    //         size_t pos = 0;
+    //         std::string token;
+    //         while ((pos = s.find("\r\n")) != std::string::npos) {
+    //             token = s.substr(0, pos);
+    //             requestlines.push_back(token);
+    //             s.erase(0, pos + 2);
+    //         }
+    //         std::cout << "Done : " << "\n";
+    //         std::vector<std::string>::iterator b = requestlines.begin();
+    //         std::vector<std::string>::iterator e = requestlines.end();
+    //         std::vector<std::vector<std::string> > jokes;
+    //         while (b != e)
+    //         {
+    //             std::vector<std::string> xx;
+    //             std::cout << "{{{{" << *b << "}}}}}}" << std::endl;
+    //             s = *b;
+    //             size_t pos = 0;
+    //             std::string token;
+    //             while ((pos = s.find(" ")) != std::string::npos || (pos = s.find("\r\n")) != std::string::npos) {
+    //                 token = s.substr(0, pos);
+    //                 xx.push_back(token);
+    //                 std::cout << "|" << token << "|";
+    //                 s.erase(0, pos + 1);
+    //             }
+    //             std::cout << "\n";
+    //             jokes.push_back(xx);
+    //             b++;
+    //         }
+
+            // exit (1);
+            // while (1) // dealing with request;
+            // {
+            //     std::string word = strtok(buffer, " ");
+            //     if (word == "GET")
+            //     {
+            //         std::cout << "GET";
+            //         std::string PATH(strtok((char *)word.c_str(), " "));
+            //         std::cout << "PATH => " << PATH << "\n";
+            //         exit (0);
+
+            //     }
+            // }
+            // else if (rec == 0)
+            //     close(rwfd);
+            // rec = recv(rwfd, buffer, 1, 0);
+            // if (rec <= 0)
+            //     break;
+            // request_text += buffer;
+            // }
+        // }
+    // }
+// 
